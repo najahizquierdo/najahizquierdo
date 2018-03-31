1f537a609d3629a68d37e2ba16790d4deea91527
Najah Izquierdo <najah.izquierdo@yahoo.com>
2018-03-31 02:12:13 -0700

    Add about me

--- src/AboutComponent.js
+++ src/AboutComponent.js
@@ -16,12 +16,52 @@ class AboutMe extends Component {
         Najah Izquierdo. 21. Creative, curious, perfectionist.
         <p> <img src={me} className="about-me-pic" /> </p>
         <p>
-          I am a web developer and a student. I attend SJSU and i'm trying to double major in
-          Economics and Computer Science. In my free time, I enjoy teaching
-          myself and learning new, creative skills that allow me to express myself in all forms.
-          I enjoy dabbling in many different areas, such as: music, digital art, and anything web design.
-          Coding has really expanded my creativity, allowing me to incorporate all of my interests due to it's versatility.
-          
+          I AM A WEB DEVELOPER AND STUDENT. </p>
+          <p>
+          I ATTEND SJSU AND I'M ATTEMPTING TO DOUBLE MAJOR IN ECONOMICS AND COMPUTER SCIENCE.
+          </p>
+          I ENJOY DABBLING IN MANY AREAS, SUCH AS: MUSIC,
+          DIGITAL ART, AND ANYTHING WEB DESIGN. IN MY FREE TIME, I ENJOY LEARNING NEW,
+          CREATIVE SKILLS THAT ALLOW ME TO further explore these interests!
+          <p>
+          web development has given me the freedom to
+          create new, exciting content within my interests.
+          Since coding is so versatile, I am able to bring life to ideas that involve both tech and art,
+          an extremely important combination. Often times, the two are categorized
+          as complete opposites. In my experience, the two must be paired in order to create beautiful and meaningful
+          content.
+          <div className="collapse">
+            <ul className="collapsible" data-collapsible="accordion">
+          <li>
+            <div className="collapsible-header white">
+              <h5>Why Coding?</h5>
+            </div>
+            <div className="collapsible-body white">
+              <div class="container">
+                From charging people $10 to completely redo their myspace layouts to attending a coding bootcamp
+                and completely falling in love with front-end development, i continue to realize that coding has
+                been a passion of mine since I was 11 years old. The versatility of coding is extremely appealing
+                because you can take it in any direction. I hope to create beautiful and meaningful projects while
+                still incorporating my personal interests and ideas.
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="collapsible-header white">
+              <h5>What about Art?</h5>
+            </div>
+            <div className="collapsible-body white">
+              <div class="container">
+                As I started to build my coding foundation, I couldn't seem to give up on my artistic outlets.
+                I've always had a deep love for music, concerts, underground warehouse parties and festivals.
+                My desire to create stems from seeing my favorite artists and DJs live. The passion and creativity that goes
+                into a live show made me curious and eager to test my abilities. This inspiration has driven me to bring my ideas to life,
+                all while being able to enjoy the feeling of contributing my art to the world.
+              </div>
+            </div>
+          </li>
+          </ul>
+        </div>
         </p>
         <p>
         <a href="http://www.facebook.com/najonaslover">
@@ -46,7 +86,7 @@ class AboutMe extends Component {
           <img src={twitter} className="icon_about" />
         </a>
     </p>
-      </div>
+</div>
     );
   }
 }
