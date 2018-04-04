067333f30e30adb2e69692517bf08eb6ac1c6a41
Najah Izquierdo <najah.izquierdo@yahoo.com>
2018-04-04 12:57:26 -0700

    Add mobile menu

--- src/App.js
+++ src/App.js
@@ -11,11 +11,65 @@ import Music from './Music.js'
 import Bsp from './BlazingStonePizza.js'
 import AboutMe from './AboutComponent.js'
 import Contact from './Contact.js'
+import me from './pictures/name-tag.png'
+
 class App extends Component {
+  constructor(props){
+   super(props);
+
+   this.state = {
+     show: false
+   }
+   this.doSomething = this.doSomething.bind(this);
+   this.toggleShow = this.toggleShow.bind(this);
+   this.hide = this.hide.bind(this);
+ }
+
+ doSomething(e){
+   e.preventDefault();
+   console.log(e.target.innerHTML);
+ }
+
+ toggleShow(){
+   this.setState({show: !this.state.show});
+ }
+
+ hide(e){
+   if(e && e.relatedTarget){
+     e.relatedTarget.click();
+   }
+   this.setState({show: false});
+ }
+
   render() {
-    return (
+        return (
       <BrowserRouter>
       <div>
+        <div className="button">
+          <a
+            className="button dropdown-toggle"
+            onClick={this.toggleShow}
+            onBlur={this.hide}
+          >
+
+            
+          {"MENU"}
+          <span className="caret"></span>
+        </a>
+          {
+            this.state.show &&
+          (
+            <ul className="dropdown-menu" style={{display: 'block'}}>
+            <li><a href="/bsp">projects</a></li>
+            <li><a href="/illustrations">Illustrations</a></li>
+            <li><a href="/moments">PHOTOGRAPHY</a></li>
+            <li><a href="/contact">contact</a></li>
+            <li><a href="/about">about</a></li>
+          </ul>
+          )
+          }
+
+        </div>
       <Sidebar />
         <Route exact path='/' component={Portfolio} />
         <Route path='/projects' component={Projects}/>
