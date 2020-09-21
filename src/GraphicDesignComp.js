9850691ac34f34a724730d8e8f25a0a2e6ff46ee
Najah <najahizquierdo@gmail.com>
2020-09-21 11:01:19 -0700

    add new updated

--- src/GraphicDesignComp.js
+++ src/GraphicDesignComp.js
@@ -9,54 +9,55 @@ class GraphicDesign extends Component {
     return (
       <div>
         <div className="container">
-    <img src={hugo} className="image"/>
-  <div className="middle">
-    <div className="text">
-      <h6>Pizza Bro</h6>
-      <p>
-        Portrait
-      </p>
-      <p>
-        Adobe Illustrator
-      </p>
-        <p>
-          2018
-        </p>
+        <img src={hugo} className="image"/>
+        <div className="middle">
+        <div className="text">
+          <h6>Pizza Bro</h6>
+          <p>
+            Portrait
+          </p>
+          <p>
+            Adobe Illustrator
+          </p>
+            <p>
+              2018
+            </p>
 
+        </div>
+        </div>
+        </div>
+      <div className="container">
+      <img src={collage} className="image"/>
+      <div className="middle">
+      <div className="text">
+        <h6>Mood Collage</h6>
+        <p>
+          Collage
+        </p>
+        <p>
+          Over and Procreate
+        </p>
+          <p>
+            2020
+          </p>
+      </div>
+      </div>
+      </div>
+      <div className="container">
+      <img src={bsp} className="image"/>
+      <div className="middle">
+      <div className="text">
+        <h6>BSP postcard</h6>
+        <p>
+          Adobe Illustrator
+        </p>
+          <p>
+            2020
+          </p>
+      </div>
+      </div>
     </div>
-  </div>
-  </div>
-   <div className="container">
-  <img src={collage} className="image"/>
-<div className="middle">
-  <div className="text">
-    <h6>Mood Collage</h6>
-    <p>
-      Collage
-    </p>
-    <p>
-      Over and Procreate
-    </p>
-      <p>
-        2020
-      </p>
-</div>
-  </div>
-</div>
-<img src={bsp} className="image"/>
-<div className="middle">
-<div className="text">
-  <h6>BSP postcard</h6>
-  <p>
-    Adobe Illustrator
-  </p>
-    <p>
-      2020
-    </p>
-</div>
-</div>
-</div>
-       </div>
+    </div>
     );
   }
 }
